c1e9d7e4e5bae2a0d19f6bdce83aa214ecff84bd
goloins <dsides.personal@gmail.com>
2026-03-24 09:01:25 -0500

    blog with initial design is done. here we diverge a bit from how the og twitheads did it, but i like this idea better.

--- blog.php
+++ blog.php
@@ -19,5 +19,28 @@ echo "<p>Here you'll find updates, news, and musings from the 4oh4oh4 team. Stay
 echo '<p>This is a mirror of our @4 account. <a href="/user/4oh4oh4">Follow us! @4oh4oh4</a></p>';
 echo '<p>...yes we do have a higher post length here on the blog. shhh ;)</p>';
 
+
+//so the plan is:
+// fetch the most recent 20 blog posts, which are just posts by the @4 account.
+$whoshouse = get_user_by_username("4");
+$runshouse = $whoshouse['id'];
+$blog_posts = get_userfeed($runshouse, 20, 0);
+
+//so unlike a typical user page with the newest post highlighted
+// in the large box atop the previous ones, each of these will 
+// just be that big box again. remember, this is a blog page, not a user profile page.
+foreach ($blog_posts as $post) {
+echo '<div class="desc">';
+echo '<p>' . $post['content'] . '</p>';
+echo '<p class="meta">';
+echo '<a href="/status/' . $post['id'] . '">' . format_time_ago($post['created_at']) . '</a>';
+echo ' from ' . $post['source'];
+echo '<span id="status_actions_' . $post['id'] . '"></span>';
+echo '</p>';
+echo '</div>';
+}
+ 
+
+
 drawfooter();
 ?>
